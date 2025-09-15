57371a2af61df7ec54a6c56e3587449fa12692b3
Giorgi Kutubidze <kutubidzegiorgi@gmail.com>
2025-09-15 11:20:08 +0400

    upload project

---
+++ HomeWork 2/Rider Solution/Rider Solution/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Homework2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+             
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.WriteLine("Giorgi Kutubidze");
+
+            Console.Write("Write something: ");
+            string input = Console.ReadLine();
+
+            Console.WriteLine(input);
+ 
+        }
+    }
+}
